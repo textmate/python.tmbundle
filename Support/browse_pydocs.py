19208af2b14f8771482947fe8d803796572a0695
Kumar McMillan <kumar@svn.textmate.org>
2005-11-14 00:03:56 +0000

    Outputs to html now so a) you can see it thinking while the server starts and b) you get some info about the server.  Specifically:  * where the log files are, including file:// links which don't actually work---probably some ignore rule on *.log?  * clickable link to the docs if you want to browse in TextMate (hmm, should we make the auto `open $url` configurable?)
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Python.tmbundle@2146 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/browse_pydocs.py
+++ Support/browse_pydocs.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# kumar dot mcmillan at gmail
 
 import os
 import sys
@@ -9,75 +10,110 @@ from urllib2 import urlopen, URLError
 PORT=9877
 URL='http://localhost:%d/' % PORT
 UID='pydoc_server_%d' % PORT
+OUT_LOG = file('/tmp/%s.log' % UID, 'a+')
+ERR_LOG = file('/tmp/%s_error.log' % UID, 'a+', 0)
 
 def browse_and_exit():
-    print "browsing pydocs at %s ..." % URL
-    sys.exit(os.system('open %s' % URL))
+    cmd = 'open %s' % URL
+    if os.system(cmd) is not 0:
+        raise OSError("failed: %s" % cmd)
+    sys.exit(0)
 
 def is_serving():
     try:
         urlopen(URL)
-        # server is up
         return True
     except URLError:
         return False
 
 def start_serv():
-    
     # Redirect standard file descriptors.
-    out_log = file('/tmp/%s.log' % UID, 'a+')
-    err_log = file('/tmp/%s_error.log' % UID, 'a+', 0)
     dev_null = file('/dev/null', 'r')
     sys.stdout.flush()
     sys.stderr.flush()
     
-    os.dup2(out_log.fileno(), sys.stdout.fileno())
-    os.dup2(err_log.fileno(), sys.stderr.fileno())
+    os.dup2(OUT_LOG.fileno(), sys.stdout.fileno())
+    os.dup2(ERR_LOG.fileno(), sys.stderr.fileno())
     os.dup2(dev_null.fileno(), sys.stdin.fileno())
     
-    # hmm, I guess the completer callback won't help me while forking
     pydoc.serve(PORT)
 
-def wait_for_server():
+def info():
+    def dd(term, d):
+        return '<dt>%s</dt><dd>%s</dd>' % (term, d)
+        
+    return """
+<style type="text/css">
+body { color: #fff; }
+h2, dt { padding: 1em 0.3em 0.3em 0.3em; }
+h2 { background: #7799ee; }
+dl { margin: 0; }
+dt { text-align: right; width: 5em; background: #ee77aa; float: left; }
+dd { background: #ffc8d8; padding-top: 1em; }
+dd a { margin-left: 0.5em; }
+h2, dd { margin-bottom: 3px; }
+dd:after {
+    content: "."; 
+    display: block; 
+    height: 0; 
+    clear: both; 
+    visibility: hidden;
+}
+</style>
+
+<h2>Pydoc Server</h2>
+<dl>
+%s
+</dl>""" % "\n".join([
+    dd('url', '<a href="%(url)s">%(url)s</a>' % {'url':URL}),
+    dd('log', '<a href="file://%(url)s">%(url)s</a>' % {'url':OUT_LOG.name}),
+    dd('error log', '<a href="file://%(url)s">%(url)s</a>' % {'url':ERR_LOG.name})])
+
+def wait_for_server(finished):
     timeout, interval, elapsed = 10,1,0
     while not is_serving():
         time.sleep(interval)
         elapsed = elapsed + interval
         if elapsed >= timeout:
             raise RuntimeError('timed out waiting for server!')
-    browse_and_exit()
+    finished()
 
 def main():
-    if is_serving():
+    def onserve():
+        print info()
         browse_and_exit()
-    
-    # the magical two forks, lifted from:
-    # http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/66012
+        
     try:
-        pid = os.fork()
-        if pid > 0:
-            print "waiting for server to start on port %d ..." % PORT
-            wait_for_server()
-            sys.exit(0)
-    except OSError, e: 
-        print >>sys.stderr, "fork #1 failed: %d (%s)" % (e.errno, e.strerror) 
-        sys.exit(1)
+        if is_serving(): onserve()
+        
+        # the magical two forks, lifted from:
+        # http://aspn.activestate.com/ASPN/Cookbook/Python/Recipe/66012
+        try:
+            pid = os.fork()
+            if pid > 0:
+                wait_for_server(onserve)
+                sys.exit(0)
+        except OSError, e: 
+            print >>sys.stderr, "fork #1 failed: %s" % (e) 
+            raise
     
-    os.chdir('/')
-    os.setsid()
-    os.umask(0)
+        os.chdir('/')
+        os.setsid()
+        os.umask(0)
     
-    try:
-        pid = os.fork()
-        if pid > 0:
-            # this is the server's pid
-            # write a pid file ?
-            sys.exit(0)
-    except OSError, e: 
-        print >>sys.stderr, "fork #2 failed: %d (%s)" % (e.errno, e.strerror) 
-        sys.exit(1)
+        try:
+            pid = os.fork()
+            if pid > 0:
+                # this is the server's pid
+                sys.exit(0)
+        except OSError, e: 
+            print >>sys.stderr, "fork #2 failed: %s" % (e)
+            raise
     
-    start_serv()
+        start_serv()
+    except:
+        print "<pre>" # ugly hack :(
+        raise
 
 if __name__ == '__main__':
     main()

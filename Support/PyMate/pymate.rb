02fbf1f973bf428641445a23134228512293fc4f
Alex Ross <alex@rosiba.com>
2006-11-18 22:14:14 +0000

    • Update PyMate to reflect changes to scriptmate.rb. • Fix a small bug in the lang grammar.
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Python.tmbundle@6180 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/PyMate/pymate.rb
+++ Support/PyMate/pymate.rb
@@ -3,10 +3,9 @@ require "#{ENV["TM_SUPPORT_PATH"]}/lib/scriptmate"
 $SCRIPTMATE_VERSION = "$Revision$"
 
 class PythonScript < UserScript
-  @@execmatch = "python(?:\d+\.\d+)?"
-  @@execargs = ['-u']
+  @@args = ['-u']
   def executable
-    @arg0 || ENV['TM_PYTHON'] || 'python'
+    @hashbang || ENV['TM_PYTHON'] || 'python'
   end
   def version_string
     res = %x{#{executable} -V 2>&1 }.chomp
@@ -15,8 +14,7 @@ class PythonScript < UserScript
 end
 
 class PyMate < ScriptMate
-  @@matename = "PyMate"
-  @@langname = "Python"
+  @@lang = "Python"
 end
 
 PyMate.new(PythonScript.new).emit_html

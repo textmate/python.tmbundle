d2fae121068f21335085c8093c51f7320e0ea8ce
Alex Ross <alex@rosiba.com>
2006-12-11 03:09:48 +0000

    • Moved "Documentation in Browser (Library Docs).tmCommand" to "Documentation for Current Word.tmCommand" as the new name is far more correct. • Bound "Documentation for Current Word" to ⌃H, and the older "Documentation in Browser" to ⌃⇧H. • More improvements to the "Documentation for Current Word" command.  It will now fall back to pydoc documentation when http://docs.python.org is not accessible, and for modules not in the standard library.  Also, if no word is currently selected; DocMate will just show the pydoc index.
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Python.tmbundle@6324 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/DocMate/docmate.py
+++ Support/DocMate/docmate.py
@@ -1,13 +1,56 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
 import re
-from os import environ as env
-from os import path
-from os import mkdir
+import sys
+from os import path, mkdir, environ as env
 import cPickle
+import urllib2
+import inspect
+from urlparse import urljoin as _urljoin
+
+# make sure Support/lib is on the path
+support_lib = path.join(env["TM_SUPPORT_PATH"], "lib")
+if support_lib not in sys.path:
+    sys.path.insert(0, support_lib)
+
+import tm_helpers
+
+if "TM_PYTHONDOCS" in env:
+    PYTHONDOCS = env["TM_PYTHONDOCS"]
+else:
+    PYTHONDOCS = "http://docs.python.org"
+
+PYDOC_PORT = 7464
+PYDOC_URL = "http://localhost:%i/" % PYDOC_PORT
 
 prefdir = path.join(env["HOME"], "Library/Preferences/com.macromates.textmate.python")
 if not path.exists(prefdir):
     mkdir(prefdir)
-hitcount_path = path.join(prefdir, 'hitcount')
+hitcount_path = path.join(prefdir, 'docmate_url_hitcount')
+
+def urljoin(base, *fragments):
+    for f in fragments:
+        base = _urljoin(base, f, allow_fragments=True)
+    return base
+
+def accessible(url):
+    """ True if the url is accessible. """
+    try:
+        urllib2.urlopen(url)
+        return True
+    except urllib2.URLError:
+        return False
+
+def load_hitcounts():
+    hit_count = {}
+    if path.exists(hitcount_path):
+        f = None
+        try:
+            f = open(hitcount_path, 'r')
+            hit_count = cPickle.load(f)
+        finally:
+            if f: f.close()
+    return hit_count
 
 def increment_hitcount(url):
     if path.exists(hitcount_path):
@@ -27,27 +70,50 @@ def increment_hitcount(url):
     finally:
         f.close()
 
+def launch_pydoc_server():
+    if not accessible(PYDOC_URL):
+        # launch pydoc.
+        sh("/usr/bin/nohup pydoc -p %i &" % PYDOC_PORT)
+    # wait until pydoc is up.
+    max_wait = 1
+    while not accessible(PYDOC_URL) and waited < max_wait:
+        time.sleep(0.05)
+        waited += 0.05
+    if not accessible(PYDOC_URL):
+        raise OSError("Could not start PyDoc server.")
+
 def doc(word):
+    """ Return a list of (desc, url) pairs for `word`. """
+    pairs = []
+    if PYTHONDOCS and accessible(PYTHONDOCS):
+        pairs = library_docs(word)
+    if accessible(PYDOC_URL):
+        pairs.extend(pydoc(word))
+    # sort by hit count.
+    if pairs:
+        h = load_hitcounts()
+        t = []
+        for desc, url in pairs:
+            t.append((h.get(url, 0), desc, url))
+        t.sort()
+        t.reverse()
+        pairs = [(desc,url) for c, desc, url in t]
+    return pairs
+
+def library_docs(word):
+    # build a list of matching library docs
+    paths = []
     try:
         f = open(path.join(env["TM_BUNDLE_SUPPORT"], 'DocMate/lib.index'))
-        words = cPickle.load(f)
+        index = cPickle.load(f)
     finally:
         f.close()
-    if path.exists(path.join(hitcount_path)):
-        try:
-            f = open(hitcount_path, 'r')
-            hit_count = cPickle.load(f)
-        finally:
-            f.close()
-    else:
-        hit_count = {}
-    paths = []
-    matching_keys = [key for key in words if re.compile(r"\b(%s)\b" % word).search(key)]
+    word_re = re.compile(r"\b(%s)\b" % word)
+    matching_keys = [key for key in index if word_re.search(key)]
     for key in matching_keys:
-        for desc, url in words[key]:
-            count = hit_count.get(url, 0)
-            paths.append((count, desc, url))
-    paths.sort()
-    paths.reverse()
-    paths = [(d,u) for c, d, u in paths]
+        for desc, url in index[key]:
+            paths.append((desc, urljoin(PYTHONDOCS, "lib/", url)))
     return paths
+
+def pydoc(word):
+    return [(word + " (pydoc)", urljoin(PYDOC_URL, "%s.html" % word))]

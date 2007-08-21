c7cf6b587ed1863622aa20c26db3cf4dc9b279c3
Jeroen van der Ham <jeroen@svn.textmate.org>
2007-08-21 11:39:44 +0000

    Shamelessly copied large parts of RubyMate to add support for unittest output parsing.
    I think I did okay, but my Python skills are far better than my Ruby skills, so please test!
    
    git-svn-id: http://svn.textmate.org/trunk/Bundles/Python.tmbundle@8018 dfb7d73b-c2ec-0310-8fea-fb051d288c6d

--- Support/PyMate/pymate.rb
+++ Support/PyMate/pymate.rb
@@ -5,21 +5,59 @@ $SCRIPTMATE_VERSION = "$Revision$"
 
 class PythonScript < UserScript
   def lang; "Python" end
-  def filter_cmd(cmd)
-    pymatepath = Pathname.new(ENV["TM_BUNDLE_SUPPORT"]) +\
-                  Pathname.new("PyMate")
-    return ["export PYTHONPATH=\"#{pymatepath}:$PYTHONPATH\";"] + cmd
-  end
   def executable; @hashbang || ENV['TM_PYTHON'] || 'python' end
   def args; ['-u'] end
   def version_string
     res = %x{#{executable} -V 2>&1 }.chomp
     res + " (#{executable})"
   end
+  def test_script?
+    @path    =~ /(?:\b|_)(?:test)(?:\b|_)/ or
+    @content =~ /\import\b.+(?:unittest)/
+  end
+  def filter_cmd(cmd)
+    pymatepath = Pathname.new(ENV["TM_BUNDLE_SUPPORT"]) +\
+                  Pathname.new("PyMate")
+    return ["export PYTHONPATH=\"#{pymatepath}:$PYTHONPATH\";"] + cmd
+  end
 end
 
 # we inherit from scriptmate just to change the classname to PyMate.
-class PyMate < ScriptMate; end
+class PyMate < ScriptMate
+  def filter_stderr(str)
+    if @command.test_script? and str =~ /\A[EF]+\Z/
+      return htmlize(str).gsub(/[EF]+/, "<span style=\"color: red\"><b>\\&</b></span>") +
+            "<br style=\"display: none\"/>"
+    elsif @command.test_script? and str =~ /\A\.\Z/
+      return htmlize(str).gsub(/\./, "<span style=\"color: green\"><b>\\&</b></span>") +
+            "<br style=\"display: none\"/>"
+    elsif @command.test_script?
+        return ( str.map do |line|
+          if line =~ /^(\s+)File (\S.+), line (\d+), in (.*)/
+            indent, file, line, method = $1, $2, $3, $4
+            url, display_name = '', 'untitled document';
+            unless file == "-"
+              indent += " " if file.sub!(/^"(.*)"/,'\1')
+              url = '&amp;url=file://' + e_url(file)
+              display_name = File.basename(file)
+            end
+            "#{indent}<a class='near' href='txmt://open?line=#{line + url}'>" +
+            (method ? "method #{CGI::escapeHTML method}" : '<em>at top level</em>') +
+            "</a> in <strong>#{CGI::escapeHTML display_name}</strong> at line #{line}<br/>"
+          elsif line =~ /^FAILED(.*)/
+            "<span style=\"color: red\"><b>FAILED#{$1}</b></span><br/>"
+          elsif line =~ /^OK(.*)/
+            "<span style=\"color: red\"><b>OK#{$1}</b></span><br/>"
+          else
+            htmlize(line)
+          end
+        end.join )
+    else
+      "<span style='color: red'>#{htmlize str}</span>".gsub(/\<br\>/, "<br>\n")
+    end
+  end
+end
+
 
 script = PythonScript.new(STDIN.read)
 PyMate.new(script).emit_html
